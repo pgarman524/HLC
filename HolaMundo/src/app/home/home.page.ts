cf1708d55a7f06b2879bab77206dbb4b5b28ddaa
pgarman524 <151721693+pgarman524@users.noreply.github.com>
2024-10-28 14:51:22 +0100

    Update home.page.ts

--- HolaMundo/src/app/home/home.page.ts
+++ HolaMundo/src/app/home/home.page.ts
@@ -6,29 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  num1: number = this.numAleatorio(1, 10);
-  num2: number = this.numAleatorio(1, 10);
-  resultado: number = this.num1 + this.num2;
-  respuestaUsuario: number | null = null;
-  mensaje: string = '';
+
+  num: number = 0;
+  numSecret: number = this.numAleatorio(1, 100);
+
+  mayorMenor: string = '...';
 
   constructor() {
-    console.log(`Pregunta: ¿Cuánto es ${this.num1} + ${this.num2}?`);
+    console.log("El numero secreto es: " + this.numSecret);
   }
 
-  numAleatorio(a: number, b: number) {
-    return Math.floor(Math.random() * (b - a + 1)) + a;
+  numAleatorio(a:number, b:number) {
+    return Math.round(Math.random() * (a - b) + parseInt(a));
   }
 
-  comprobarRespuesta() {
-    if (this.respuestaUsuario === this.resultado) {
-      this.mensaje = '¡Correcto! 🎉';
-    } else {
-      this.mensaje = `Incorrecto. La respuesta correcta es ${this.resultado}.`;
+  compruebaNumero() {
+    if (this.num) {
+      if (this.numSecret < this.num) {
+        this.mayorMenor = 'menor';
+      } else if (this.numSecret > this.num) {
+        this.mayorMenor = 'mayor';
+      } else {
+        this.mayorMenor = 'igual';
+      }
     }
-
-    this.num1 = this.numAleatorio(1, 10);
-    this.num2 = this.numAleatorio(1, 10);
-    this.resultado = this.num1 + this.num2;
-    this.respuestaUsuario = null;
   }
+
+}
